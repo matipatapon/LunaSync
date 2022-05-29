7f82aa7bf2b4246a74a7c80c05ea639fdfe0e719
Itam <matipietka@gmail.com>
2022-05-29 16:39:18 +0200

    Added Button scratch

--- go.rb
+++ go.rb
@@ -1,28 +1,47 @@
 module Clickable
-  @@clickable_objects = []
-  @clickable = true
-  puts "I am #{self}"
+  Clickable_objects = []
+  def clicked(mx,my)
+    if @go.contains?(mx,my) and @active then @method.call(self) end
+  end
+  def set_click_method method
+    @method = method
+  end
 end
 
 class GraphicObject
+  attr_reader :go
+
   def initialize(posx: -1,posy: 0,height: 100,width: 100,color: "white")
-    puts "I am #{self} and i am #{self.class.include? Clickable}"
+    
     @active = true
+    
     @go = Rectangle.new(
     x: posx, y: posx,
     width: width, height: height, color: color
-    ) #Child graphic object 
+    )
+
+    #Child graphic objects 
     @slave_go = [] 
+
+    #Reporting this instance to clicable objects array 
+    if self.class.include? Clickable then Clickable::Clickable_objects << self end 
+    
   end    
   
   #Create new under graphical object
-  def add(what: "GraphicObject",posx: 0,posy: 0,height: 100,width: 100,color: "black",text: "whatever")
+  def add(what: "GraphicObject",posx: 0,posy: 0,z: @go.z+1,height: 100,width: 100,color: "black",text: "whatever",object: nil,method: nil)
+    new = nil
     case what
     when "GraphicObject"
-      @slave_go << GraphicObject.new(posx: posx,posy: posy,height: height, width:width, color: color)
+      new = GraphicObject.new(posx: posx,posy: posy,height: height, width:width, color: color)
     when "Textobj"
-      @slave_go << Textobj.new(posx: posx, posy: posy,color: color , text: text)
+      new = Textobj.new(posx: posx, posy: posy,color: color , text: text)
+    when "Buttonobj"
+      go = Buttonobj.new(posx: posx, posy: posy, color: color)
+      go.set_click_method(method)
+      new = go
     end
+    return new
     
   end
 
@@ -64,12 +83,20 @@ end
 
 #Text object
 class Textobj < GraphicObject 
+  
   def initialize(posx: -1,posy: 0,color: "white",text: "whatever",size:20)
-    @active = true 
-    @go = Text.new(
-      text, x: posx , y:posx ,color: color , size:size)
+    @active = true
+    @go = Text.new(text, x: posx , y:posx ,color: color , size:size)
     @slave_go = []
-    
+  end
+  
+  def change_text(text = "Whatever")
+    posx = @go.x
+    posy = @go.y
+    color = @go.color
+    size = @go.size  
+    @go.remove 
+    @go = Text.new(text,x: posx,y: posy, color: color , size: size)
   end
 end
 
--- gui.rb
+++ gui.rb
@@ -7,15 +7,32 @@ require_relative 'go'
 
 screen = GraphicObject.new(width: Window.width,height: Window.height)
 screen.add(what: "Textobj")
+screen.hide
 tick = 0 
-button = Buttonobj.new
+
+text = Textobj.new(:color => "red")
+
 update do
-  case 
-  when (tick%60) === 0
-    screen.hide
-  when (tick%60) === 30
-    screen.show
-  end
+  text.change_text((tick%60).to_s)
   tick+=1
 end 
+
+#Mouse interaction 
+on :mouse_down do |event|
+  for co in Clickable::Clickable_objects
+    co.clicked(Window.mouse_x,Window.mouse_y)
+  end
+end
+
+rainbowclick = Proc.new do |obj|
+  puts "Fairly tail power of friendship !!!!"
+  hex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+  color = "#"
+  6.times do 
+    color+=hex[rand(15)]
+  end
+  obj.go.color = color
+end
+butt = screen.add(what: "Buttonobj",method: rainbowclick)
+butt2 = butt.add(what: "Textobj",color: "white")
 show
