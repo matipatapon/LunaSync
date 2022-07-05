fe0d09aa4fc6c5c4dfaf825932692d5ec27f2c58
Itam <matipietka@gmail.com>
2022-07-06 01:47:50 +0200

    Made a raw version of GETINFO command This command will be used to obtain information about files by client from server.

--- server/servamp.cs
+++ server/servamp.cs
@@ -49,15 +49,24 @@ abstract public class hostshared{
                 break;
             }
                 foreach(var f in d.EnumerateFiles()){
-                    log.l($"{f.Name} found");
+
                     var fi = new file(f.FullName,dir.FullName);
                     //#1 Get info about file
+                    log.l($"Master : sending GETINFO");
                     chandler.sendText("GETINFO");
-                    
+                    log.l($"Master : waiting for response");
                     var response = chandler.receiveText();
-                    if(response == "OK"){
-                        chandler.sendText(fi.localPath);
+                    log.l($"Master : got response {response}");
+                    if(response != "OK"){
+                        log.l($"Master : Wrong response {response}",log.level.error);
+                        throw new ArgumentException($"Wrong response {response}");
                     }
+                    log.l($"Master : sending localPath{fi.localPath}");
+                    chandler.sendText(fi.localPath);
+
+                    var rInfo = chandler.receiveText();
+                    log.l($"Master : received info{rInfo}");
+                    WriteLine($"Got {rInfo} ^^");
 
                 }
             enumerateThroughSubdirs(d);
@@ -78,15 +87,32 @@ abstract public class hostshared{
             log.l("chandler is null !",log.level.error);
             throw new ArgumentNullException("chandler is null !");
         }
+        if(fhandler is null){
+            log.l("fhandler is null !",log.level.error);
+            throw new ArgumentNullException("fhandler is null");
+        }
         while(true){
             string command = chandler.receiveText();
             switch(command){
                 case "GETINFO":
+                    log.l("Slave : GETINFO sending OK");
                     chandler.sendText("OK");
+                    //#1 get local path to the file 
                     var localPath = chandler.receiveText();
                     var dir = fhandler.getDirectory().FullName;
                     var pathToFile = dir.Substring(0,dir.Length-1)+localPath;
                     WriteLine($"{pathToFile}");
+                    log.l($"Slave crafted path to the file {pathToFile}");
+                    //#2 Check if this file exist 
+                    bool exist = File.Exists(pathToFile);
+                    //#3 if exist get info about file and send it if not send "nogamenofile"
+                    string info = "nogamenofile";
+                    if(exist){
+                        info = new file(pathToFile,dir).ToString();
+                    }
+                    log.l($"Slave sending back info {info}");
+                    chandler.sendText(info);
+                    
 
                 break;
             }
